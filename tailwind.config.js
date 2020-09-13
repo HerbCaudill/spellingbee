9e70c4fe04aeb754d4e7f3593335e55c990007f9
HerbCaudill <herb@devresults.com>
2020-09-13 17:24:58 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+}
