fcf55c0e93658143633cd4b9c460ff9b8e19ea66
HerbCaudill <herb@devresults.com>
2020-09-13 17:09:42 +0200

    memoize solutions

--- src/lib/solutions.ts
+++ src/lib/solutions.ts
@@ -1,9 +1,10 @@
 ﻿import { distinctLetters } from './distinctLetters'
 import words from '../data/en.json'
+import memoize from 'fast-memoize'
 
-export const solutions = (letters: string) => {
+export const solutions = memoize((letters: string) => {
   if (letters.length !== 7) throw new Error('A game has exactly 7 letters')
   if (distinctLetters(letters).length !== 7) throw new Error('All 7 letters must be distinct')
   const keyLetter = letters[0]
   return words.filter(w => w.includes(keyLetter) && w.split('').every(l => letters.includes(l)))
-}
+})
