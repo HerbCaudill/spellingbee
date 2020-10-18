165a551cadd7636ee80c896193e8f46d1f724e4d
HerbCaudill <herb@devresults.com>
2020-10-18 19:39:08 +0200

    lib/getScore.ts: create

---
+++ src/lib/getScore.ts
@@ -0,0 +1,8 @@
+﻿import { isPangram } from './isPangram'
+
+export const getScore = (word: string) => {
+  if (word.length < 4) return 0
+  if (word.length === 4) return 1
+  const bonus = isPangram(word) ? 7 : 0
+  return word.length + bonus
+}
