560b40801b0f0001d80b6d60dab5b46d04fcc40b
HerbCaudill <herb@devresults.com>
2020-10-18 19:39:14 +0200

    lib/score.ts: remove

--- src/lib/score.ts
+++
@@ -1,8 +0,0 @@
-﻿import { isPangram } from './isPangram'
-
-export const score = (word: string) => {
-  if (word.length < 4) return 0
-  if (word.length === 4) return 1
-  const bonus = isPangram(word) ? 7 : 0
-  return word.length + bonus
-}
