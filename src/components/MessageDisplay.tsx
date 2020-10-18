787d90d7ae520586375f98ef1bef88f3a9e25fc8
HerbCaudill <herb@devresults.com>
2020-10-18 18:20:46 +0200

    components/MessageDisplay.tsx:

--- src/components/MessageDisplay.tsx
+++ src/components/MessageDisplay.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Message } from '../Types'
+import { makeRandom } from '../lib/makeRandom'
+import { Message } from '../types'
+
+const random = makeRandom('1')
 
 export const MessageDisplay = ({ message }: { message: Message | undefined }) => {
   const getDisplayMessage = (message: Message): string => {
@@ -7,9 +10,13 @@ export const MessageDisplay = ({ message }: { message: Message | undefined }) =>
       case Message.ALREADY_FOUND:
         return 'Already found!'
       case Message.BAD_LETTERS:
-        return 'Bad letters!'
+        return 'Bad letters'
       case Message.GOOD:
-        return 'Good!!'
+        return 'Good!'
+      case Message.EXCELLENT:
+        return random.pick(['Nice!', 'Excellent!', 'Sweet!'])
+      case Message.PANGRAM:
+        return 'Pangram!'
       case Message.MISSING_KEY:
         return 'Missing center letter!'
       case Message.NOT_A_WORD:
