f097556bee1d4097db024fac4be86955f17baa08
HerbCaudill <herb@devresults.com>
2020-10-18 19:39:21 +0200

    components/FoundWords.tsx:

--- src/components/FoundWords.tsx
+++ src/components/FoundWords.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export const FoundWords = ({ words }: { words: string[] }) => {
   return (
     <div className="border border-gray-300 my-5 p-5 rounded-md">
-      <p>
+      <p className="my-2">
         You have found {words.length} word{words.length === 1 ? '' : 's'}
       </p>
-      {words.map((word, i) => (
-        <p key={i} className="border-b border-gray-300 w-6/12">
+      {words.sort().map((word, i) => (
+        <p key={i} className="my-2 border-b border-gray-300 w-6/12">
           {word}
         </p>
       ))}
